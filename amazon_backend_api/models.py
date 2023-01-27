380a6ed8bb2fc2b69e23d02da17e2211aa486fa5
Aashish <aashishkumar12376@gmail.com>
2023-01-27 17:03:51 +0530

    [MODIFY] USER.

--- amazon_backend_api/models.py
+++ amazon_backend_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from amazon_backend_api.manager import AmazonuserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Amazonuser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = AmazonuserManager()
+
+    def __str__(self):
+        return self.email
--- backend/settings.py
+++ backend/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#REGISTER CUSTOM USER
+AUTH_USER_MODEL = 'amazon_backend_api.Amazonuser'
