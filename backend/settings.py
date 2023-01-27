5c85d16d30de3bbb606427a4aa869e4ae50eb029
Aashish <aashishkumar12376@gmail.com>
2023-01-27 16:35:13 +0530

    [SETUP] postgres db.

--- backend/settings.py
+++ backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'amazon-backend-db',
+        'USER': 'postgres',
+        'PASSWORD': 'aakumar123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
