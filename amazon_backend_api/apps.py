c8422773a9a642d03d93a6e16e7530ee1dd5b281
Aashish <aashishkumar12376@gmail.com>
2023-01-27 16:17:01 +0530

    [SETUP] amazon-backend-api app.

---
+++ amazon_backend_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AmazonBackendApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'amazon_backend_api'
--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'amazon_backend_api.apps.AmazonBackendApiConfig',
 ]
 
 MIDDLEWARE = [
