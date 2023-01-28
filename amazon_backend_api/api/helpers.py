8db9143ca4cc475a0ab694a7cdcf81e4c979ccda
Aashish <aashishkumar12376@gmail.com>
2023-01-28 15:40:49 +0530

    [CREATE] function that will get get user from token.

--- amazon_backend_api/api/helpers.py
+++ amazon_backend_api/api/helpers.py
@@ -1,4 +1,8 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+JWT_authenticator = JWTAuthentication()
 
 
 def get_tokens_for_user(user):
@@ -10,3 +14,10 @@ def get_tokens_for_user(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+
+
+def get_user_from_token(request):
+    response = JWT_authenticator.authenticate(request)
+    if response is not None:
+        user , token = response
+        return user
