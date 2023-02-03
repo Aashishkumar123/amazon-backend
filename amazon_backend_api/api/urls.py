872ec1f449d3a802c605253f8cc02ba6134d5ebc
Aashish <aashishkumar12376@gmail.com>
2023-02-03 14:02:40 +0530

    [CREATE] api to get the access token from refresh token.

--- amazon_backend_api/api/urls.py
+++ amazon_backend_api/api/urls.py
@@ -1,5 +1,23 @@
-from django.urls import path
+from django.urls import path,re_path
 from amazon_backend_api.api import views
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 urlpatterns = [
@@ -12,4 +30,8 @@ urlpatterns = [
     path('products/fashion/<str:subcategory1>/<str:subcategory2>/',views.ProductsAPIView.as_view(),name="amz-products-subcategory2"),
     path('product/product-id/<int:product_id>/product-detail/<int:product_detail_id>/',views.ProductDetailsAPIView.as_view(),name="amz-product-details"),
     path('cart/',views.CartAPIView.as_view(),name="amz-cart"),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
     'amazon_backend_api.apps.AmazonBackendApiConfig',
 ]
 
