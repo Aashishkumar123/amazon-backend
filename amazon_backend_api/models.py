6f1ab389fbc6af12a6f9de52e5ba4852e5bff22b
Aashish <aashishkumar12376@gmail.com>
2023-02-09 19:40:12 +0530

    [REF] api/models.py file.

--- amazon_backend_api/models.py
+++ amazon_backend_api/models.py
@@ -5,17 +5,17 @@ from django.utils.translation import gettext_lazy as _
 from amazon_backend_api.api.utils import INDIAN_STATES
 
 
-STATE_CHOICES = ( (state,INDIAN_STATES.get(state)) for state in INDIAN_STATES )
+STATE_CHOICES = ((state, INDIAN_STATES.get(state)) for state in INDIAN_STATES)
 
 
 class Amazonuser(AbstractUser):
     username = None
     first_name = None
     last_name = None
-    email = models.EmailField(_('email address'), unique=True)
-    full_name = models.CharField(_('full name'), max_length=100)
+    email = models.EmailField(_("email address"), unique=True)
+    full_name = models.CharField(_("full name"), max_length=100)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = AmazonuserManager()
@@ -36,92 +36,125 @@ class BaseModel(models.Model):
 class UserAddress(BaseModel):
 
     user = models.ForeignKey(Amazonuser, on_delete=models.CASCADE, verbose_name="User")
-    country = models.CharField(verbose_name='Country', choices=(('India','India'),), max_length=100)
-    full_name = models.CharField(verbose_name='Full name (First and last name)', max_length=100)
-    mobile_number = models.IntegerField(verbose_name='Mobile number')
-    pincode = models.IntegerField(verbose_name='Pincode')
-    flat = models.CharField(verbose_name='Flat, House no., Building, Company, Apartment', max_length=200)
-    street = models.CharField(verbose_name='Area, Street, Sector, Village', max_length=200)
-    landmark = models.CharField(verbose_name='Landmark', max_length=100)
-    town = models.CharField(verbose_name='Town/City', max_length=100)
-    state = models.CharField(verbose_name='State', choices=STATE_CHOICES, max_length=100)
-    default = models.BooleanField(verbose_name='Make this my default address', default=False)
-    address_type = models.CharField(verbose_name='Address Type', max_length=100, choices=(('Home','Home'),('Office','Office')))
-
+    country = models.CharField(
+        verbose_name="Country", choices=(("India", "India"),), max_length=100
+    )
+    full_name = models.CharField(
+        verbose_name="Full name (First and last name)", max_length=100
+    )
+    mobile_number = models.IntegerField(verbose_name="Mobile number")
+    pincode = models.IntegerField(verbose_name="Pincode")
+    flat = models.CharField(
+        verbose_name="Flat, House no., Building, Company, Apartment", max_length=200
+    )
+    street = models.CharField(
+        verbose_name="Area, Street, Sector, Village", max_length=200
+    )
+    landmark = models.CharField(verbose_name="Landmark", max_length=100)
+    town = models.CharField(verbose_name="Town/City", max_length=100)
+    state = models.CharField(
+        verbose_name="State", choices=STATE_CHOICES, max_length=100
+    )
+    default = models.BooleanField(
+        verbose_name="Make this my default address", default=False
+    )
+    address_type = models.CharField(
+        verbose_name="Address Type",
+        max_length=100,
+        choices=(("Home", "Home"), ("Office", "Office")),
+    )
 
     def __str__(self):
-        return str(f'{self.full_name} Address')
+        return str(f"{self.full_name} Address")
 
 
 class Brand(BaseModel):
-    name = models.CharField(verbose_name='Brand Name', max_length=100)
-    logo = models.ImageField(upload_to='brands/logo')
+    name = models.CharField(verbose_name="Brand Name", max_length=100)
+    logo = models.ImageField(upload_to="brands/logo")
 
     def __str__(self):
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class Category(BaseModel):
-    name = models.CharField(verbose_name='Category Name', max_length=100)
+    name = models.CharField(verbose_name="Category Name", max_length=100)
 
     def __str__(self):
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class Subcategory(BaseModel):
-    name = models.CharField(verbose_name='SubCategory Name', max_length=100)
+    name = models.CharField(verbose_name="SubCategory Name", max_length=100)
 
     def __str__(self):
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class Size(BaseModel):
-    name = models.CharField(verbose_name='Size', max_length=100)
+    name = models.CharField(verbose_name="Size", max_length=100)
 
     def __str__(self):
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class Color(BaseModel):
-    name = models.CharField(verbose_name='Color Name', max_length=100)
-    code = models.CharField(verbose_name='Color Code', max_length=100)
+    name = models.CharField(verbose_name="Color Name", max_length=100)
+    code = models.CharField(verbose_name="Color Code", max_length=100)
 
     def __str__(self):
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class Product(BaseModel):
-    name = models.CharField(verbose_name='Product Name', max_length=200)
-    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, related_name='Brand_Name', null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='Category', null=True)
-    subcategory1 = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, related_name='SubCategory1', null=True)
-    subcategory2 = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, related_name='SubCategory2', null=True)
+    name = models.CharField(verbose_name="Product Name", max_length=200)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.SET_NULL, related_name="Brand_Name", null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name="Category", null=True
+    )
+    subcategory1 = models.ForeignKey(
+        Subcategory, on_delete=models.SET_NULL, related_name="SubCategory1", null=True
+    )
+    subcategory2 = models.ForeignKey(
+        Subcategory, on_delete=models.SET_NULL, related_name="SubCategory2", null=True
+    )
 
     def __str__(self) -> str:
-        return str(f'{self.name}')
+        return str(f"{self.name}")
 
 
 class ProductDetail(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-    size = models.ManyToManyField(Size, related_name='size')
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True, related_name='Color')
-    description = models.TextField(verbose_name='Product Descriptions', max_length=1000)
-    mrp = models.DecimalField(verbose_name='MRP', decimal_places=2, max_digits=10)
-    discount = models.DecimalField(verbose_name='Discount', decimal_places=2, max_digits=10)
-    stocks =models.IntegerField(verbose_name='Number of Stocks')
-    is_stock = models.BooleanField(verbose_name='Stocks Avaliable', default=True)
-    image1 = models.ImageField(upload_to='products',verbose_name='Image 1')
-    image2 = models.ImageField(upload_to='products',verbose_name='Image 2')
-    image3 = models.ImageField(upload_to='products',verbose_name='Image 3')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product"
+    )
+    size = models.ManyToManyField(Size, related_name="size")
+    color = models.ForeignKey(
+        Color, on_delete=models.SET_NULL, null=True, related_name="Color"
+    )
+    description = models.TextField(verbose_name="Product Descriptions", max_length=1000)
+    mrp = models.DecimalField(verbose_name="MRP", decimal_places=2, max_digits=10)
+    discount = models.DecimalField(
+        verbose_name="Discount", decimal_places=2, max_digits=10
+    )
+    stocks = models.IntegerField(verbose_name="Number of Stocks")
+    is_stock = models.BooleanField(verbose_name="Stocks Avaliable", default=True)
+    image1 = models.ImageField(upload_to="products", verbose_name="Image 1")
+    image2 = models.ImageField(upload_to="products", verbose_name="Image 2")
+    image3 = models.ImageField(upload_to="products", verbose_name="Image 3")
 
     def __str__(self) -> str:
-        return str(f'{self.product} Details')
+        return str(f"{self.product} Details")
 
 
 class Cart(BaseModel):
-    user = models.ForeignKey(Amazonuser, on_delete=models.CASCADE, related_name='AmazonUser')
-    product = models.ForeignKey(ProductDetail, on_delete=models.CASCADE, related_name='Product')
-    quantity = models.IntegerField(verbose_name='Quantity', default=1)
+    user = models.ForeignKey(
+        Amazonuser, on_delete=models.CASCADE, related_name="AmazonUser"
+    )
+    product = models.ForeignKey(
+        ProductDetail, on_delete=models.CASCADE, related_name="Product"
+    )
+    quantity = models.IntegerField(verbose_name="Quantity", default=1)
 
     def __str__(self):
-        return str(f'{self.product}')
+        return str(f"{self.product}")
