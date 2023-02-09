8189d2eb2465e695f5a17327d803822d8248e005
Aashish <aashishkumar12376@gmail.com>
2023-02-09 19:34:42 +0530

    [REF] helper.py file.

--- amazon_backend_api/api/helpers.py
+++ amazon_backend_api/api/helpers.py
@@ -1,36 +1,37 @@
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.utils.translation import gettext_lazy as _
 from amazon_backend_api.models import Amazonuser
 
 
 JWT_authenticator = JWTAuthentication()
-        
+
 
 def get_tokens_for_user(user):
-    
+
     refresh = RefreshToken.for_user(user)
-    
+
     return {
-        'email' : user.email,
-        'full name' : user.full_name,
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
+        "email": user.email,
+        "full name": user.full_name,
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
     }
 
 
 def get_user_from_token(request):
+
     response = JWT_authenticator.authenticate(request)
+
     if response is not None:
-        user , token = response
+        user, token = response
         return user
 
 
 def get_access_token_from_refresh_token(request):
 
-    if request.data['grant_type'] == 'refresh_token':
-        access_token = RefreshToken(request.data['refresh_token']).access_token
-        request.META['HTTP_AUTHORIZATION'] = 'Bearer ' + str(access_token)
+    if request.data["grant_type"] == "refresh_token":
+        access_token = RefreshToken(request.data["refresh_token"]).access_token
+        request.META["HTTP_AUTHORIZATION"] = "Bearer " + str(access_token)
         user = get_user_from_token(request)
         data = get_tokens_for_user(Amazonuser.objects.get(email=user))
         return data
