ed2b22522d2b4240e2a1bb87b1b9385e6727e230
Aashish <aashishkumar12376@gmail.com>
2023-02-19 16:44:43 +0530

    [REMOVE] create method from AmazonuserAddressSerializer.

--- amazon_backend_api/api/serializers.py
+++ amazon_backend_api/api/serializers.py
@@ -37,9 +37,6 @@ class AmazonuserAddressSerializer(serializers.ModelSerializer):
 
     user = serializers.CharField(source="user.email", read_only=True)
 
-    def create(self, validated_data):
-        return UserAddress.objects.create(**validated_data)
-
     class Meta:
         model = UserAddress
         fields = ALL_FIELDS
